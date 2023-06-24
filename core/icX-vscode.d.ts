60971c18c7836ea526b56cbb7e2c133345e5321f
Kirill <traineratwot@yandex.ru>
2023-06-24 11:39:32 +0300

    1

--- core/icX-vscode.d.ts
+++
@@ -1,4 +0,0 @@
-import { Ic10Vscode } from "./ic10-vscode";
-export declare class IcXVscode extends Ic10Vscode {
-    wiki: string;
-}
--- debugger/utils.d.ts
+++
@@ -1,2 +0,0 @@
-import InterpreterIc10 from "ic10";
-export declare function parseEnvironment(ic10: InterpreterIc10, file: string): string;
