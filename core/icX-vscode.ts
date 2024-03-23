8ce3e166bd014eccf33dbff90c947f43cdd318b3
kirill <traineratwot@yandex.ru>
2024-03-23 10:38:12 +0300

    Очистка

--- core/icX-vscode.ts
+++
@@ -1,6 +0,0 @@
-"use strict"
-import {Ic10Vscode} from "./ic10-vscode"
-
-export class IcXVscode extends Ic10Vscode {
-    public wiki = "https://icx.traineratwot.site/wiki/icX"
-}
--- debugger/activateIc10Debug.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from "vscode"
-import {FileAccessor} from "./ic10Runtime"
-
-export declare function activateIc10Debug(context: vscode.ExtensionContext, factory?: vscode.DebugAdapterDescriptorFactory): void;
-
-export declare const workspaceFileAccessor: FileAccessor
